e50ba9d444d69a996e1dffb602f4d1c9a60a7d5f
Rai <rai.lima17@live.com>
2017-06-13 13:11:14 -0300

    __

--- src/br/edu/theproject/molde/Atividade.java
+++ src/br/edu/theproject/molde/Atividade.java
@@ -5,17 +5,26 @@ public class Atividade {
 	private String nome;
 	private double valor;
 	
+<<<<<<< HEAD
+	public int getId() {
+		return id;
+	}
+=======
 	public Atividade(String nome, double valor) {
 		this.nome = nome;
 		this.valor = valor;
 	}
 	
+<<<<<<< HEAD
 	public Atividade(int id, String nome, double valor) {
 		this.id = id;
 		this.nome = nome;
 		this.valor = valor;
 	}
 	
+=======
+>>>>>>> 6d57bc76b503c3c48c95efa98904d677ebcecce7
+>>>>>>> 290326f25ac46b3d179bd78f19913e1cc14d6036
 	public String getNome() {
 		return nome;
 	}
@@ -25,6 +34,17 @@ public class Atividade {
 
 	public int getId() {
 		return id;
+	}
+	
+	public Atividade(String nome, double valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+	
+	public Atividade(int id, String nome, double valor) {
+		this.id = id;
+		this.nome = nome;
+		this.valor = valor;
 	}
 	
 }
